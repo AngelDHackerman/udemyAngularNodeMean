e96e3a191ac0a896380efe9a2e6362853f503cd2
angelDariaux <angeldariaux@gmail.com>
2022-03-25 21:15:52 -0600

    plantillas y propiedades

--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -2,19 +2,17 @@ import { Component } from "@angular/core";
 
 @Component ({
   selector: 'videojuego',
-  template: `
-    <h2>Componente de videojuego</h2>
-    <ul>
-      <li>GTA</li>
-      <li>Price of Persia</li>
-      <li>Tekken</li>
-      <li>Mario</li>
-    </ul>
-  `
+  templateUrl: './videojuego.component.html' // ? Asi es como traemos el codigo del archivo videojuego.component.html
 })
 
 export class VideojuegoComponent {
+  public titulo: string;
+  public listado: string;
+
   constructor (){
+    this.titulo = 'Componente de videojuegos';
+    this.listado = 'Listado de los juegos mas populares';
+
     console.log('Se ha cargado el componente: videojuego.Component.ts')
   }
 }
