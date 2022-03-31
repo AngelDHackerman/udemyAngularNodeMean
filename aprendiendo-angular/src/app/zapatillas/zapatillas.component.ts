c99d7a961821306d1e69c7b532d067581cafb0c6
angelDariaux <angeldariaux@gmail.com>
2022-03-30 18:26:25 -0600

    usando ngFor para recorred los arrays

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -11,16 +11,29 @@ import { Zapatillas } from "../models/zapatilla";
 export class ZapatillasComponent implements OnInit{
   public titulo: string = 'Componente de zapatillas';
   public zapatillas: Array<Zapatillas>; // ? zapatillas es de tipo array de tipo zapatillas
+  public marcas: String[];
 
   constructor () {
     this.zapatillas = [
       new Zapatillas('Nike Airmax', 'Nike', 'Rojas', 190, true),
       new Zapatillas('Reebook Classic', 'Reebook', 'Blanco', 80, true), // ? Esto es una clase, un molde para las zapatillas
+      new Zapatillas('Rebook Spartan', 'Reebook', 'Negra', 180, true),
       new Zapatillas('Nike Runner D', 'Nike', 'Negro', 60, true),
       new Zapatillas('Adidas Yezzy', 'Adidas', 'Gris', 180, false)
     ];
+    this.marcas = new Array;
   }
   ngOnInit() {
       console.log(this.zapatillas);
+      this.getMarcas();
+  }
+
+  getMarcas () {
+    this.zapatillas.forEach((zapatilla, index) => {
+      if (this.marcas.indexOf(zapatilla.marca) < 0) { // ? Si el elemento buscado por indexOf dentro de zapatilla.marca == 0, (osea no existe y es primera ver que pasa por el indexOf)
+        this.marcas.push(zapatilla.marca); // ? si no existe pues le damos push, si en dado caso ya existe no le dara push.
+      }
+    });
+    console.log(this.marcas);
   }
 }
