ef5d500c2078486678e0e43389c6c3fc30e95c1b
angelDariaux <angeldariaux@gmail.com>
2022-03-31 19:53:11 -0600

    comenzando a configurar el routing de angular

---
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,13 @@
+// * Importar modulos del router de angular
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+// * Importar componentes
+import { HomeComponent } from "./home/home.component";
+import { ZapatillasComponent } from "./zapatillas/zapatillas.component";
+import { VideojuegoComponent } from "./videojuego/videojuego.component";
+import { CursosComponent } from "./cursos/cursos.component";
+
+// * Array de rutas (aqui vamos a dar de alta las rutas y configurar las rutas)
+
+
