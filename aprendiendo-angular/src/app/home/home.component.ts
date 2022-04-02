f53b63e1b28a0f99bb3756ad5277e858142eee89
angelDariaux <angeldariaux@gmail.com>
2022-04-02 10:17:31 -0600

    usando el if and else en angular

--- aprendiendo-angular/src/app/home/home.component.ts
+++ aprendiendo-angular/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public identificado: boolean;
 
-  ngOnInit(): void {
+  constructor() {
+    this.identificado = false;
+   }
+
+  ngOnInit(){
+
+  }
+  setIdentificado () {
+    this.identificado = true;
+  }
+  unsetIdentificado () {
+    this.identificado = false;
   }
 
 }
