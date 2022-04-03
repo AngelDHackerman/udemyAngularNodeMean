0495087fe39c9890882d516a1e3065eefb148d86
angelDariaux <angeldariaux@gmail.com>
2022-04-03 17:01:57 -0600

    creando peticiones por get y por post

--- backEnd/app.js
+++ backEnd/app.js
@@ -21,13 +21,13 @@ app.use(bodyParser.json()); // todo lo que le llegue a body parser lo convertira
 
 // * Rutas
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // ? app.get significa que haremos peticiones por get
   res.status(200).send(
     "<h1>Pagina de inicio</h1>"
   )
 })
 
-app.get('/test', (req, res) => { 
+app.post('/test', (req, res) => { // ? app.post, haremos las peticiones por post
   res.status(200).send({ // ? El estatus 200 significa que se logro con exito.
     message: "Hola mundo desde mi API de NodeJs"
   })
