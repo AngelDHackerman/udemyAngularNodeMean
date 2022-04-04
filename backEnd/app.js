a537ac02e531be0517f29a88d0000fd41a4bf888
angelDariaux <angeldariaux@gmail.com>
2022-04-04 17:48:47 -0600

    viendo los post request con nodemon y postman

--- backEnd/app.js
+++ backEnd/app.js
@@ -28,6 +28,9 @@ app.get('/', (req, res) => { // ? app.get significa que haremos peticiones por g
 })
 
 app.post('/test', (req, res) => { // ? app.post, haremos las peticiones por post
+  console.log(req.body.nombre) // ? al hacer la solicitud con postman podemos recoger el nombre que le pasamos por x-www-form-urlencoded
+  console.log(req.query.web);
+  // console.log(req.params.id)
   res.status(200).send({ // ? El estatus 200 significa que se logro con exito.
     message: "Hola mundo desde mi API de NodeJs"
   })
